f1549dcc740175b7da8100e5ae651a494ce40827
cleverdog <motoki@cleverdog.me>
2022-01-16 15:40:31 +0900

    Spreadsheeet control importing data from excel

---
+++ SpecificFunctions/spreadsheetFromXlsx/ReadMe.md
@@ -0,0 +1,23 @@
+<!-- NOTE: do not change this file because it's auto re-generated from: -->
+<!-- https://github.com/IgniteUI/igniteui-wc-examples/tree/master/templates/browser/sample/ReadMe.md -->
+
+エクセルファイルを読み込んでSpreadsheetコンポーネントに反映させるサンプルです。
+
+In terminal window, run:
+
+```
+npm install
+npm run start
+```
+
+<!--
+This project was created with [IgniteUI/create-webcomponents-app](https://github.com/IgniteUI/create-webcomponents-app). -->
+
+## Scripts
+
+| Command         | Description  |
+|-----------------|--------------|
+| `npm run start` | Runs the app locally for modern browsers. |
+| `npm run start:legacy` | Runs the app locally for legacy browsers (IE11) |
+| `npm run build` | Builds the app for production (modern browsers) |
+| `npm run build:legacy` | Builds the app for production (legacy browsers) |
---
+++ SpecificFunctions/spreadsheetFromXlsx/src/index.ts
@@ -0,0 +1,45 @@
+import { IgcSpreadsheetModule } from 'igniteui-webcomponents-spreadsheet';
+import { IgcSpreadsheetComponent } from 'igniteui-webcomponents-spreadsheet';
+import { ModuleManager } from 'igniteui-webcomponents-core';
+import { Workbook } from 'igniteui-webcomponents-excel';
+import { WorkbookOptionsBase, WorkbookFormat } from 'igniteui-webcomponents-excel';
+import { ExcelUtility } from './ExcelUtility';
+
+ModuleManager.register(IgcSpreadsheetModule);
+
+export class SpreadsheetOverview {
+
+    private spreadsheet: IgcSpreadsheetComponent;
+
+    constructor() {
+
+        this.spreadsheet = document.getElementById('spreadsheet') as IgcSpreadsheetComponent;
+
+        let path = 'https://static.infragistics.com/xplatform/excel/SalesData.xlsx';
+        ExcelUtility.loadFromUrl(path).then((w) => {
+            this.spreadsheet.workbook = w;
+        });
+
+        let fileInput = document.getElementById('files');
+        fileInput!.addEventListener('change', this.onFileRead);
+    }
+
+    public onFileRead = (e: any) => {
+        let input = e.target;
+        if (input.files.length == 0) {
+            console.log('No file selected');
+            return;
+        }
+        const file = input.files[0];
+        const reader = new FileReader();
+        reader.onload = () => {
+            ExcelUtility.load(file).then((w) => {
+                this.spreadsheet.workbook = w;
+            });
+        };
+        reader.readAsText(file);
+    }
+
+}
+
+new SpreadsheetOverview();
